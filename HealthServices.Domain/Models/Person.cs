2ffe7f7ea13726bc4e7d702083f5692e3a2425f7
Eddie Rangel <hacker@eddierangel.com>
2014-04-26 22:19:44 -0700

    Created Domain Model
    Person

---
+++ HealthServices.Domain/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HealthServices.Domain.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
