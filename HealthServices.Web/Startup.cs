93ee448ce01c895411d02a9a0291086094f132ad
Eddie Rangel <hacker@eddierangel.com>
2014-04-26 22:08:40 -0700

    Initial Check In
    Creating Base Projects for Enterprise Architecture

---
+++ HealthServices.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HealthServices.Web.Startup))]
+namespace HealthServices.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
