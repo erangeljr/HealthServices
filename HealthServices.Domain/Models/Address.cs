da2312c5a945fb95d44bdbc6906fe6de467d3a74
Eddie Rangel <hacker@eddierangel.com>
2014-04-27 22:21:42 -0700

    Create Models
    Address
    Provider

---
+++ HealthServices.Domain/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthServices.Domain.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string AddressOne { get; set; }
+        public string AddressTwo { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZIP { get; set; }
+    }
+}
---
+++ HealthServices.Domain/Models/Provider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthServices.Domain.Models
+{
+    public class Provider
+    {
+        public int ProviderId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Address> Addresses { get; set; }
+
+
+    }
+}
